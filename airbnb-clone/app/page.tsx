748d3476426ac9e3d547c96569932a71030e5da9
Nimish Lingesh <111670449+NimishLingesh@users.noreply.github.com>
2023-11-10 06:31:32 -0800

    Loading spinner

--- airbnb-clone/app/page.tsx
+++ airbnb-clone/app/page.tsx
@@ -1,4 +1,10 @@
+import LoadingSpinner from "./components/LoadingSpinner";
 
 export default function Home() {
   return <div className="text-rose-500 text-2xl">Airbnb</div>
+  // return (
+  //   <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+  // <LoadingSpinner isLoading={true}/>
+  //   </div>
+  // )
 }
