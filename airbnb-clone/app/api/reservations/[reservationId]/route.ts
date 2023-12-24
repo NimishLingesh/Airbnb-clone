f6635af82f7e6e8504f89dae208ef2b60b069579
Nimish Lingesh <111670449+NimishLingesh@users.noreply.github.com>
2023-12-23 18:06:41 -0800

    lint fixes

--- airbnb-clone/app/api/reservations/[reservationId]/route.ts
+++ airbnb-clone/app/api/reservations/[reservationId]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-import getCurrentUser from "@/app/actions/getCurrentUser";
+import getCurrentUser from "@/app/actions/getCurrentUSer";
 import prisma from "@/app/libs/prismadb";
 
 interface IParams {
